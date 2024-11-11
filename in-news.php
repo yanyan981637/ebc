aa9799a91476554600f2c09a6d703a516cb4284a
yanyan.lin <yanyan.lin@mitacmdt.com>
2024-11-11 10:11:14 +0800

    add a in news

--- in-news.php
+++ in-news.php
@@ -203,6 +203,22 @@ include_once("analyticstracking.php");
 								<article class="entry col-12 mb-3">
 									<div class="grid-inner row gutter-20">
 										<div class="col-md-1">
+											<a href="https://embeddedcomputing.com/application/industrial/smart-energy-utilities/deploying-ai-in-agriculture-applications-to-enhance-efficiency-and-cultivate-healthier-crops-for-a-sustainable-future" target="min"><img src="/images/pressroom_pic/PRIM-embedded.gif" alt=""  ></a>
+										</div>
+										<div class="col-md-11">
+											<div class="entry-title title-xs">
+												<span class="badge rounded-pill bg-info">2024/10/28</span><br>
+												<h2><a href="https://embeddedcomputing.com/application/industrial/smart-energy-utilities/deploying-ai-in-agriculture-applications-to-enhance-efficiency-and-cultivate-healthier-crops-for-a-sustainable-future" target="min">Deploying AI in Agriculture Applications to Enhance Efficiency and Cultivate Healthier Crops for a Sustainable Future</a></h2>
+												<!--Contents--><span></span><!--end Contents-->
+											</div>
+										</div>
+									</div>
+								</article>
+							<!--end one -->
+							<!--one -->
+								<article class="entry col-12 mb-3">
+									<div class="grid-inner row gutter-20">
+										<div class="col-md-1">
 											<a href="https://embeddedcomputing.com/technology/ai-machine-learning/computer-vision-speech-processing/deploy-ai-for-smart-retail-with-mitac-ma1" target="min"><img src="/images/pressroom_pic/PRIM-embedded.gif" alt=""  ></a>
 										</div>
 										<div class="col-md-11">
